679ae44b136c07e568262d9b090cdec0724b8833
zac948 <zac@idealutahhomes.com>
2026-08-29 22:15:16 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('r1').then(c=>c.addAll(['./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
